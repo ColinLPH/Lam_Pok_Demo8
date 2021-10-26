c165943bea7bd66e454b6b86ba9f5cc2867eb73c
Colin Lam <colin.lph.lam@gmail.com>
2021-10-26 12:17:27 -0700

    completed demo8

--- scripts/firebaseAPI.js
+++ scripts/firebaseAPI.js
@@ -1,13 +1,13 @@
 // Your web app's Firebase configuration
-var  firebaseConfig = {
-    apiKey: ,
-    authDomain: ",
-    projectId: ,
-    storageBucket: ,
-    messagingSenderId: ,
-    appId: 
-  };
-  
+var firebaseConfig = {
+  apiKey: "AIzaSyDqunF4lnyZOu9ID9jS3KPJbgDKlbD_XXU",
+  authDomain: "demo8-comp1800-3e930.firebaseapp.com",
+  projectId: "demo8-comp1800-3e930",
+  storageBucket: "demo8-comp1800-3e930.appspot.com",
+  messagingSenderId: "287496196511",
+  appId: "1:287496196511:web:612406ccf2573e1b752daa",
+  measurementId: "G-MXQLWCGSBR"
+};
 
-  const app = firebase.initializeApp(firebaseConfig);
-  
+
+const app = firebase.initializeApp(firebaseConfig);
